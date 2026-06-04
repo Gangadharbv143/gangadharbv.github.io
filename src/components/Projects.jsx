1280ac49b20eb5821fcd0a3ab0c9ac4ac55872a6
Gangadhar <gangadhargangadharbv@gmail.com>
2026-06-04 13:03:25 +0000

    Added Monitoring Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -65,6 +65,26 @@ function Projects() {
           </a>
         </div>
 
+        <div className="bg-gray-900 p-6 rounded-xl shadow-lg">
+          <h3 className="text-2xl font-semibold mb-3">
+            Prometheus & Grafana Monitoring Project
+          </h3>
+
+          <p className="mb-4">
+            Built a monitoring stack using Prometheus and Grafana. Configured metrics collection,
+            monitoring dashboards and infrastructure observability.
+          </p>
+
+          <a
+            href="https://github.com/Gangadharbv143/monitoring-project"
+            target="_blank"
+            rel="noreferrer"
+            className="text-cyan-400"
+          >
+            View Project
+          </a>
+        </div>
+
       </div>
     </section>
   );
