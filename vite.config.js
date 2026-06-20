6559b5a98b1104e7b9c4a189beda0c25f7b6af67
Gangadhar <gangadhargangadharbv@gmail.com>
2026-06-20 15:23:59 +0000

    fix deployment base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+
+  // IMPORTANT: change "portfolio" to your repo name if different
+  base: "/portfolio/",
 })
