9fde112692726d4fe438c8adddeda93c6a00e3fa
Gangadhar <gangadhargangadharbv@gmail.com>
2026-06-20 15:40:19 +0000

    fix blank page deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // IMPORTANT: change "portfolio" to your repo name if different
-  base: "/portfolio/",
+  // IMPORTANT FIX FOR ALL GITHUB / STATIC DEPLOYS
+  base: "./",
 })
